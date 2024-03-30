858c1718166fa26a2f04e7d867f2bc58d4945153
wissamserhan <116583162+wissamserhan@users.noreply.github.com>
2024-03-30 21:22:12 +0000

    mi proyecto

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,57 @@
-//import react into the bundle
+
+
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-// include your styles into the webpack bundle
+
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function SimpleCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="reloj">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="cuatro">{props.digitoCuatro}</div>
+      <div className="tres">{props.digitoTres}</div>
+      <div className="dos">{props.digitoDos}</div>
+      <div className="uno">{props.digitoUno}</div>
+    </div>
+  );
+}
+
+
+SimpleCounter.propTypes = {
+  digitoCuatro: PropTypes.number,
+  digitoTres: PropTypes.number,
+  digitoDos: PropTypes.number,
+  digitoUno: PropTypes.number,
+};
+
+
+function startCounter() {
+  let counter = 0;
+  setInterval(() => {
+    const cuatro = Math.floor(counter / 1000) % 10;
+    const tres = Math.floor(counter / 100) % 10;
+    const dos = Math.floor(counter / 10) % 10;
+    const uno = Math.floor(counter / 1) % 10;
+
+    ReactDOM.render(
+      <SimpleCounter
+        digitoUno={uno}
+        digitoDos={dos}
+        digitoTres={tres}
+        digitoCuatro={cuatro}
+      />,
+      document.querySelector("#app")
+    );
+
+    counter++;
+  }, 1000);
+}
+
+
+startCounter();
